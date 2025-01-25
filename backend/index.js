a456ff4f1baa7a1321ff89f4ba357e80a8d06231
Kamal <dragkamal71@gmail.com>
2025-01-25 23:08:22 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -229,7 +229,3 @@ app.post("/api/logout", async (req, res) => {
 }); 
 
 // Ensure the 'uploads' directory exists
-const UPLOADS_DIR = path.join(__dirname, "./uploads"); 
-if (!fs.existsSync(UPLOADS_DIR)) {
-  fs.mkdirSync(UPLOADS_DIR);
-}
