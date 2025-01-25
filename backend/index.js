238626f6f16e0ca445544e24a0bd8f90fcd8a21f
Kamal <dragkamal71@gmail.com>
2025-01-25 20:27:48 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -26,10 +26,7 @@ const connection = async () => {
 };
 connection();
 const corsOptions = {
-  origin: ["http://localhost:5173" , "https://suvam-ivory.vercel.app"], // Allow frontend to access the backend
-  methods: "GET,POST,PUT,DELETE", // Specify allowed HTTP methods
-  allowedHeaders: "Content-Type,Authorization", // Specify allowed headers
-  credentials: true,
+  origin: "*", // Allow frontend to access the backend
 };
 
 // Use CORS middleware
